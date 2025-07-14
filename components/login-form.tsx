616629c33e54d49d1c522c1894c9345c2d632dde
Marmik soni <marmiksoni1182002@gmail.com>
2025-07-14 16:15:35 +0530

    password

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -112,6 +112,7 @@ export default function LoginForm() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
+              placeholder="Enter your password"
             />
           </div>
           {error && (
