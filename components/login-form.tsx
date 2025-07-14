ac4b48879b0cec30728648e94dcd6e898835b9a2
Marmik soni <marmiksoni1182002@gmail.com>
2025-07-14 16:15:03 +0530

    checking...

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -149,12 +149,12 @@ export default function LoginForm() {
           </Button> */}
         </div>
       </div>
-      <div className="mt-4 text-center text-sm">
+      {/* <div className="mt-4 text-center text-sm">
         Don&apos;t have an account?{" "}
         <Link href="/signup" className="underline">
           Sign up
         </Link>
-      </div>
+      </div> */}
 
       {/* <Card className="mt-4">
         <CardHeader className="pb-3">
