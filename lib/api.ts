d72e8a60f43d78a356fe3d045aba221a64895ff4
Marmik soni <marmiksoni1182002@gmail.com>
2025-07-18 20:54:30 +0530

    api changes

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,8 @@
 // API Configuration
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_LOCAL_URL
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
-const API_BASE_URL = "http://localhost:5000/api"
+// const API_BASE_URL = "http://localhost:5000/api"
 
 // Define custom URLs for specific endpoints
 const CUSTOM_ENDPOINTS = {
